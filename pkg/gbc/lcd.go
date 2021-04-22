c8d911b910873b02a918640bfad9a03d44476d28
pokemium <bluejapan73+dev@gmail.com>
2021-04-23 00:53:44 +0900

    fix

--- pkg/gbc/lcd.go
+++ pkg/gbc/lcd.go
@@ -4,9 +4,8 @@ import "gbc/pkg/util"
 
 func (cpu *CPU) setHBlankMode() {
 	cpu.mode = HBlankMode
-	STAT := cpu.FetchMemory8(LCDSTATIO)
-	STAT &= 0b1111_1100
-	cpu.SetMemory8(LCDSTATIO, STAT)
+	stat := cpu.FetchMemory8(LCDSTATIO) & 0b1111_1100
+	cpu.SetMemory8(LCDSTATIO, stat)
 
 	if cpu.GPU.HBlankDMALength > 0 {
 		cpu.doVRAMDMATransfer(0x10)
@@ -19,33 +18,30 @@ func (cpu *CPU) setHBlankMode() {
 		}
 	}
 
-	if util.Bit(STAT, 3) {
+	if util.Bit(stat, 3) {
 		cpu.setLCDSTATFlag(true)
 	}
 }
 
 func (cpu *CPU) setVBlankMode() {
 	cpu.mode = VBlankMode
-	STAT := cpu.FetchMemory8(LCDSTATIO)
-	STAT = (STAT | 0x01) & 0xfd // bit0-1: 01
-	cpu.SetMemory8(LCDSTATIO, STAT)
+	stat := (cpu.FetchMemory8(LCDSTATIO) | 0x01) & 0xfd // bit0-1: 01
+	cpu.SetMemory8(LCDSTATIO, stat)
 }
 
 func (cpu *CPU) setOAMRAMMode() {
 	cpu.mode = OAMRAMMode
-	STAT := cpu.FetchMemory8(LCDSTATIO)
-	STAT = (STAT | 0x02) & 0xfe // bit0-1: 10
-	cpu.SetMemory8(LCDSTATIO, STAT)
-	if util.Bit(STAT, 5) {
+	stat := (cpu.FetchMemory8(LCDSTATIO) | 0x02) & 0xfe // bit0-1: 10
+	cpu.SetMemory8(LCDSTATIO, stat)
+	if util.Bit(stat, 5) {
 		cpu.setLCDSTATFlag(true)
 	}
 }
 
 func (cpu *CPU) setLCDMode() {
 	cpu.mode = LCDMode
-	STAT := cpu.FetchMemory8(LCDSTATIO)
-	STAT |= 0b11
-	cpu.SetMemory8(LCDSTATIO, STAT)
+	stat := cpu.FetchMemory8(LCDSTATIO) | 0b11
+	cpu.SetMemory8(LCDSTATIO, stat)
 }
 
 func (cpu *CPU) incrementLY() {
@@ -57,23 +53,21 @@ func (cpu *CPU) incrementLY() {
 	}
 	LY %= 154 // LY = LY >= 154 ? 0 : LY
 	cpu.RAM[LYIO] = LY
-	cpu.compareLYC(LY)
+	cpu.checkLYC(LY)
 }
 
-func (cpu *CPU) compareLYC(LY uint8) {
+func (cpu *CPU) checkLYC(LY uint8) {
 	LYC := cpu.FetchMemory8(LYCIO)
 	if LYC == LY {
-		// LCDC STAT IOポートの一致フラグをセットする
-		STAT := cpu.FetchMemory8(LCDSTATIO) | 0x04
-		cpu.SetMemory8(LCDSTATIO, STAT)
+		stat := cpu.FetchMemory8(LCDSTATIO) | 0x04 // set lyc flag
+		cpu.SetMemory8(LCDSTATIO, stat)
 
-		if util.Bit(STAT, 6) {
+		if util.Bit(stat, 6) { // trigger LYC=LY interrupt
 			cpu.setLCDSTATFlag(true)
 		}
 		return
 	}
 
-	// LCDC STAT IOポートの一致フラグをクリアする
-	STAT := cpu.FetchMemory8(LCDSTATIO) & 0b11111011
-	cpu.SetMemory8(LCDSTATIO, STAT)
+	stat := cpu.FetchMemory8(LCDSTATIO) & 0b11111011 // clear lyc flag
+	cpu.SetMemory8(LCDSTATIO, stat)
 }
