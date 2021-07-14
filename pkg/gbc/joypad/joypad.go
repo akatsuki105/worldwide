1bc772419a639f712b33677de24db8335c5bd6bd
pokemium <bluejapan73+dev@gmail.com>
2021-07-14 09:17:33 +0900

    fix

--- pkg/gbc/joypad/joypad.go
+++ pkg/gbc/joypad/joypad.go
@@ -20,11 +20,6 @@ type Joypad struct {
 	handler           [8](func() bool)
 }
 
-const (
-	Pressed = iota + 1
-	Pause
-)
-
 func New(h [8](func() bool)) *Joypad {
 	return &Joypad{
 		handler: h,
@@ -36,16 +31,12 @@ func (pad *Joypad) Output() byte {
 	joypad := byte(0x00)
 	if p15 := !util.Bit(pad.P1, 5); p15 {
 		for i := 0; i < 4; i++ {
-			if pad.button[i] {
-				joypad |= (1 << uint(i))
-			}
+			joypad = util.SetBit8(joypad, i, pad.button[i])
 		}
 	}
 	if p14 := !util.Bit(pad.P1, 4); p14 {
 		for i := 0; i < 4; i++ {
-			if pad.direction[i] {
-				joypad |= (1 << uint(i))
-			}
+			joypad = util.SetBit8(joypad, i, pad.direction[i])
 		}
 	}
 	return ^joypad
@@ -55,92 +46,30 @@ func (pad *Joypad) Output() byte {
 func (j *Joypad) Input() bool {
 	pressed := false
 
-	// A
-	if j.handler[A]() {
-		old := j.button[0]
-		j.button[0] = true
-		if !old && j.button[0] {
-			pressed = true
+	// A,B,Start,Select
+	for i := 0; i < 4; i++ {
+		if j.handler[i]() {
+			old := j.button[i]
+			j.button[i] = true
+			if !old && j.button[i] {
+				pressed = true
+			}
+		} else {
+			j.button[i] = false
 		}
-	} else {
-		j.button[0] = false
 	}
 
-	// B
-	if j.handler[B]() {
-		old := j.button[1]
-		j.button[1] = true
-		if !old && j.button[1] {
-			pressed = true
+	// Right, Left, Up, Down
+	for i := 0; i < 4; i++ {
+		if j.handler[i+4]() {
+			old := j.direction[i]
+			j.direction[i] = true
+			if !old && j.direction[i] {
+				pressed = true
+			}
+		} else {
+			j.direction[i] = false
 		}
-	} else {
-		j.button[1] = false
-	}
-
-	// select
-	if j.handler[Select]() {
-		old := j.button[2]
-		j.button[2] = true
-		if !old && j.button[2] {
-			pressed = true
-		}
-	} else {
-		j.button[2] = false
-	}
-
-	// start
-	if j.handler[Start]() {
-		old := j.button[3]
-		j.button[3] = true
-		if !old && j.button[3] {
-			pressed = true
-		}
-	} else {
-		j.button[3] = false
-	}
-
-	// right
-	if j.handler[Right]() {
-		old := j.direction[0]
-		j.direction[0] = true
-		if !old && j.direction[0] {
-			pressed = true
-		}
-	} else {
-		j.direction[0] = false
-	}
-
-	// left
-	if j.handler[Left]() {
-		old := j.direction[1]
-		j.direction[1] = true
-		if !old && j.direction[1] {
-			pressed = true
-		}
-	} else {
-		j.direction[1] = false
-	}
-
-	// up
-	if j.handler[Up]() {
-		old := j.direction[2]
-		j.direction[2] = true
-		if !old && j.direction[2] {
-			pressed = true
-		}
-	} else {
-		j.direction[2] = false
-	}
-
-	// down
-	if j.handler[Down]() {
-		old := j.direction[3]
-		j.direction[3] = true
-		if !old && j.direction[3] {
-			pressed = true
-		}
-	} else {
-		j.direction[3] = false
 	}
 
 	return pressed
